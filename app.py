6a55d1a93a3df84a62e16f3f17648663e5112a80
maliabarker <89502882+maliabarker@users.noreply.github.com>
2021-10-29 11:57:00 -0700

    Users can see single playlists

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ db = client.Playlistr
 playlists = db.playlists
 
 from flask import Flask, render_template, request, redirect, url_for
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-
 def video_url_creator(id_lst):
     videos = []
     for vid_id in id_lst:
+        # We know that embedded YouTube videos always have this format
         video = 'https://youtube.com/embed/' + vid_id
         videos.append(video)
-        print(videos)
     return videos
 
 @app.route('/')
@@ -26,10 +26,13 @@ def playlists_new():
     """Create new playlist"""
     return render_template('playlists_new.html')
 
+# Note the methods parameter that explicitly tells the route that this is a POST
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
-    """Submit new playlist"""
-    video_ids = str(request.form.get('video_ids')).split()
+    """Submit a new playlist."""
+    # Grab the video IDs and make a list out of them
+    video_ids = request.form.get('video_ids').split()
+    # call our helper function to create the list of links
     videos = video_url_creator(video_ids)
     playlist = {
         'title': request.form.get('title'),
@@ -38,8 +41,13 @@ def playlists_submit():
         'video_ids': video_ids
     }
     playlists.insert_one(playlist)
-    print(request.form.to_dict())
     return redirect(url_for('playlists_index'))
+
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    """Show single playlist"""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist)
 
 
 if __name__ == '__main__':
